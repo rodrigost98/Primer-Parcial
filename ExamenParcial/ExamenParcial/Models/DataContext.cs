feec0a01ad43e4fd91d626dbc42446b7bd40fd32
visual_community@outlook.com <visual_community@outlook.com>
2019-09-28 02:44:42 -0300

    Segundo Commit

---
+++ ExamenParcial/ExamenParcial/Models/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Entity;
+
+namespace ExamenParcial.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+    }
+}
